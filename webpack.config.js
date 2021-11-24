68d798d98c6a445ecf74e906e45bbcc3f7b05fac
Raz-Dva <my.poshta.goncharov@gmail.com>
2021-11-24 16:47:27 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: 'development',
+  // watch: true,
+  entry: path.resolve(__dirname + '/src/index.js'),
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true
+    }),
+    new CopyWebpackPlugin([
+      { from: './src/assets', to: 'assets' }
+    ]
+    )
+  ],
+  devServer: {
+    // contentBase: path.join(__dirname, 'src'),
+    compress: true,
+    // watchContentBase: true,
+    port: 4200
+  }
+}
